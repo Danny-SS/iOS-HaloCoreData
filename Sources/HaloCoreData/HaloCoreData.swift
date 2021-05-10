6a1ad9275678492af7d1ca07de73fdb8e88bed79
DannyJM <31970539+Danny-SS@users.noreply.github.com>
2021-05-10 16:32:10 -0400

    Test Adding platforms back into Package.swift

--- Sources/HaloCoreData/HaloCoreData.swift
+++ Sources/HaloCoreData/HaloCoreData.swift
@@ -1,3 +1,11 @@
+import os
+
+private let subsystem = "com.halocommunications.HaloCoreData"
+
+struct Log {
+    static let utilities = OSLog(subsystem: subsystem, category: "HaloCoreData")
+}
+
 struct HaloCoreData {
     var text = "Hello, World!"
 }
